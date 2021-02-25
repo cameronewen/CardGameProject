f1d64aff2d6958b1a89775afe49248d4d44e375b
damia <damia@MSI.myfiosgateway.com>
2021-02-25 13:36:36 -0800

    It's working

--- CardGameProject/src/CardGamePackage/Main.java
+++ CardGameProject/src/CardGamePackage/Main.java
@@ -3,7 +3,7 @@ package CardGamePackage;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");// TODO Auto-generated method stub
+		System.out.println("Welcome to BlackJack");// TODO Auto-generated method stub
 
 	}
 
