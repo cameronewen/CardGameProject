6dff53afe00016e38615a69f343c28621dd6e676
Mykel <withwaterllc@gmail.com>
2021-02-25 13:50:04 -0800

    Here's a change

--- CardGameProject/src/CardGamePackage/Main.java
+++ CardGameProject/src/CardGamePackage/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to BlackJack");// TODO Auto-generated method stub
-        //My'kel testing upload
+        //testing upload
 	}
 
 }
