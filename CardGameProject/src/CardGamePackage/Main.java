4676e2542b43942b72ead7bb4843e9873a0d23a1
Mykel <withwaterllc@gmail.com>
2021-02-25 19:52:02 -0800

    Moved menu if-else statement to menu() function.

--- CardGameProject/src/CardGamePackage/Main.java
+++ CardGameProject/src/CardGamePackage/Main.java
@@ -4,23 +4,14 @@ import java.util.Scanner;
 public class Main {
 	
 	public static void  Menu() {
+		Scanner scnr = new Scanner(System.in);
+		int menuOption; 
+		
 		System.out.println("Menu:");
 		System.out.println("> Gameplay and Rules (1)\n"
 				+ "> Deal hand (2)\n" + "> Exit (3)\n");
 		
-		
-	}
-
-	public static void main(String[] args) {
-		Scanner scnr = new Scanner(System.in);
-		int menuOption; 
-		
-		
-		System.out.println("Welcome to BlackJack\n");
-        
-		Menu();
-		
-		menuOption = scnr.nextInt(); 
+        menuOption = scnr.nextInt(); 
 		
 		if (menuOption == 1) {
 			System.out.println("> Gameplay and Rules \n");
@@ -31,6 +22,19 @@ public class Main {
 		} else {
 			System.out.println ("Exit \n");
 		}
+		
+		scnr.close(); 
+		
+	}
+
+	public static void main(String[] args) {
+	
+		
+		
+		System.out.println("Welcome to BlackJack\n");
+        
+		Menu();
+	
 	}
 	
 	
