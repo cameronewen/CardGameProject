18a45e630e1c5df5f5461e8fbc65c4ec56fc9806
Cameron <cameronefay@gmail.com>
2021-02-27 12:30:19 -0800

    Created more connections between Hand and Player

--- CardGameProject/src/CardGamePackage/Hand.java
+++ CardGameProject/src/CardGamePackage/Hand.java
@@ -7,16 +7,62 @@ public class Hand {
 // FIELDS
 	
 	private ArrayList<Card> hand = new ArrayList<Card>();
-	private int value;
-	private boolean hasAce = false;
-	private boolean isBusted = false;
+	private int value;						// total value of cards in hand
+	private boolean hasAce = false;			// does the hand have an ace?
+	private boolean isBusted = false; 		// hand value goes over 21
+	private boolean doubledDown = false; 	// no more cards can be added to hand
+	private boolean isActive = true; 		// player surrender (diff from bust)
+	private int timesSplit = 0; 			// number of times hand has been split
+	private int wager;						// the players wager on this hand
 	
+// CONSTRUCTOR
+	
+	public Hand() {
+		
+		value = 0;
+		
+	}
+	
+	public Hand(Card card1) {
+		
+		hand.add(card1);
+		
+	}
 	
 // METHODS
 	
 	public void addCard(Card newCard) {
 		
-		hand.add(newCard);
+		if(!doubledDown) {
+			
+			hand.add(newCard);
+			
+		} else {
+			
+			System.out.println("This hand has been doubled down, and no more cards can be added.");
+			
+		}
+		
+		countValue();
+		
+	}
+	
+	public void removeCard(int index) {
+		
+		hand.remove(index);
+		
+	}
+	
+	public void countValue() {
+		
+		// TODO fill in with value logic wrt aces.
+		
+	}
+	
+	public void doubleDown() {
+		
+		doubledDown = true;
+		wager *= 2;
 		
 	}
 
@@ -63,6 +109,54 @@ public class Hand {
 		
 		this.isBusted = isBusted;
 		
+	}
+	
+	// card checks
+	
+	public Card getCard(int index) {
+		
+		return hand.get(index);
+				
+	}
+	
+	public int getSize() {
+		
+		return hand.size();
+		
+	}
+	
+	// number of times a hand has been split
+
+	public int getTimesSplit() {
+		
+		return timesSplit;
+		
+	}
+
+	public void setTimesSplit(int timesSplit) {
+		
+		this.timesSplit = timesSplit;
+		
+	}
+	
+	// wager on the current hand
+
+	public int getWager() {
+		return wager;
+	}
+
+	public void setWager(int wager) {
+		this.wager = wager;
+	}
+	
+	// double down
+
+	public boolean isDoubledDown() {
+		return doubledDown;
+	}
+
+	public void setDoubledDown(boolean doubledDown) {
+		this.doubledDown = doubledDown;
 	}
 	
 	
--- CardGameProject/src/CardGamePackage/Player.java
+++ CardGameProject/src/CardGamePackage/Player.java
@@ -1,5 +1,55 @@
 package CardGamePackage;
 
-public class Player {
+import java.util.ArrayList;
 
+public class Player { // this is not the user, this class includes npcs
+
+// FIELDS
+	
+	private ArrayList<Hand> playerHands = new ArrayList<Hand>(); // players can have multiple hands thanks to splitting
+	private String name;
+
+	
+// CONSTRUCTOR
+	
+	public Player(String name) {
+		
+		playerHands.add(new Hand());
+		this.name = name;
+		
+	}
+	
+// METHODS
+	
+	public void splitHand(Hand handToSplit) {
+		// if the hands has only two cards AND the hand has been split less than twice AND the cards are equal
+		if( (handToSplit.getSize() == 2) && 
+			(handToSplit.getTimesSplit() < 2) &&
+			(handToSplit.getCard(0).getValue() == handToSplit.getCard(1).getValue()) ) {
+			
+			Hand newHand = new Hand(handToSplit.getCard(1)); // add 2nd card to its own new hand
+			handToSplit.removeCard(1); // remove card from source hand
+			
+			handToSplit.setTimesSplit(handToSplit.getTimesSplit() + 1); // incrementing the times a hand has been split
+			newHand.setTimesSplit(newHand.getTimesSplit() + 1);
+			
+			playerHands.add(newHand); // add the new hand to the players hands
+			
+		} else {
+			
+			System.out.println("You can't split this hand!"); // this shouldn't happen in game, option shouldn't be offered if they can't split
+			
+		}
+	}
+	
+// GETTERS AND SETTERS
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String playerName) {
+		this.name = playerName;
+	}
+	
 }
