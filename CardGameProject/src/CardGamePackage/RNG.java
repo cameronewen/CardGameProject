c57e02eb4607eb1122c3620416cb2342edc2ee3f
Mykel <withwaterllc@gmail.com>
2021-03-06 13:33:31 -0800

    Starting to build random number generator

---
+++ CardGameProject/src/CardGamePackage/RNG.java
@@ -0,0 +1,29 @@
+package CardGamePackage;
+
+public class RNG {
+	
+	
+	public RNG() {
+	
+	//This is a Random Number Generator used to shuffle the cards in the deck
+	// Xsubn-1 = (aXsubn + b) mod m
+	}
+	
+	public static int RandomNumberGenerator(int i) {
+		//Test zone open
+		
+		int x = i%51;
+		
+		
+		return x;
+		
+		
+		
+		
+		
+		//Test zone closed
+		
+		
+	}
+	
+	}
