c0d1c7f06c29e128bf1b28d2b3c49500c9549a9c
Cam <cameronefay@gmail.com>
2021-03-08 21:29:41 -0800

    Fixed splitting, added surrender method

--- CardGameProject/src/CardGamePackage/Player.java
+++ CardGameProject/src/CardGamePackage/Player.java
@@ -26,8 +26,9 @@ public class Player { // this is not the user, this class includes npcs
 	public void splitHand(Hand handToSplit) {
 		// if the hands has only two cards AND the hand has been split less than twice AND the cards are equal
 		if( (handToSplit.getSize() == 2) && 
-			(handToSplit.getTimesSplit() < 2) &&
-			(handToSplit.getCard(0).getValue() == handToSplit.getCard(1).getValue()) ) {
+			(handToSplit.getTimesSplit() < Hand.maxSplits) &&
+			( 	(handToSplit.getCard(0).getValue() 	== handToSplit.getCard(1).getValue()) || 
+				(handToSplit.getCard(0).getRank() 	== handToSplit.getCard(1).getRank())  )){
 			
 			Hand newHand = new Hand(handToSplit.getCard(1)); // add 2nd card to its own new hand
 			handToSplit.removeCard(1); // remove card from source hand
@@ -36,20 +37,33 @@ public class Player { // this is not the user, this class includes npcs
 			newHand.setTimesSplit(newHand.getTimesSplit() + 1);
 			
 			handToSplit.addCard(Main.currentGame.getDeck().dealCard()); // adds a new card to each hand from the deck
-			newHand.addCard(Main.currentGame.getDeck().dealCard()); // FIXME if/when the deck is moved to a permanent spot, change this
+			newHand.addCard(Main.currentGame.getDeck().dealCard()); 
 			
 			playerHands.add(newHand); // add the new hand to the players hands
 			
 		} else {
 			
-			System.out.println("You can't split this hand!"); // this shouldn't happen in game, option shouldn't be offered if they can't split
+			System.out.println("DEBUG: You can't split this hand!"); // this shouldn't happen in game, option shouldn't be offered if they can't split
+			
+			if( !(handToSplit.getTimesSplit() < Hand.maxSplits) ) {
+				System.out.println("DEBUG: handToSplit.getTimesSplit() < Hand.maxSplits");
+			}
+			
+			if(handToSplit.getCard(0).getValue() != handToSplit.getCard(1).getValue()) {
+				System.out.println("DEBUG: card 1 doesnt have same value as card 2");
+			}
 			
 		}
 		
 	}
 
-	public void Stand(Hand handToStand) {
+	public void stand(Hand handToStand) {
 		handToStand.setHasBeenStoodOn(true);
+	}
+	
+	public void surrender(Hand handToSurrender) {
+		handToSurrender.setActive(false);
+		System.out.println("DEBUG: give player back 1/2 wager");
 	}
 	
 // GETTERS AND SETTERS
@@ -60,6 +74,14 @@ public class Player { // this is not the user, this class includes npcs
 
 	public void setName(String playerName) {
 		this.name = playerName;
+	}
+	
+	public Hand getHand(int i) {
+		return playerHands.get(i);
+	}
+	
+	public Card getCardFromHand(int handIndex, int cardIndex) {
+		return playerHands.get(handIndex).getCard(cardIndex);
 	}
 	
 }
