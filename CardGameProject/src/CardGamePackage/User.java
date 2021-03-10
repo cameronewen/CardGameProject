1aeab534c87816e7222ffe089f23906ab2916caf
Cam <cameronefay@gmail.com>
2021-03-10 13:51:42 -0800

    Added totalMoney field, secondaray constructor

--- CardGameProject/src/CardGamePackage/User.java
+++ CardGameProject/src/CardGamePackage/User.java
@@ -1,10 +1,20 @@
 package CardGamePackage;
 
 public class User extends Player{
+	
+	public int totalMoney;
 
+	public User(String name, int money) {
+		
+		super(name);
+		this.totalMoney = money;
+		
+	}
+	
 	public User(String name) {
 		
 		super(name);
+		this.totalMoney = 100;
 		
 	}
 
