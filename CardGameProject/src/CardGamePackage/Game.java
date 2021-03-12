8be99123640bbbb805cdf76b362bd94c9a3e9799
Cam <cameronefay@gmail.com>
2021-03-11 20:42:45 -0800

    testing / fixing the wager system

--- CardGameProject/src/CardGamePackage/Game.java
+++ CardGameProject/src/CardGamePackage/Game.java
@@ -36,41 +36,56 @@ public class Game {
 		Random rng = new Random();
 		
 		System.out.println("Wagers\n\nMinimum Wager: $" + Main.minWager + "\nMaximum Wager: $" + Main.maxWager);
-		System.out.println("\nCurrent Wallet: $" + Main.playerMoney + "\n");
+		System.out.println("\nCurrent Wallet: $" + user.getMoney() + "\n");
 		
 	
 		if(Main.playerMoney < Main.minWager) { // if they dont have enough to make min bet, add min wager to their account.
 			
 			System.out.println("You don't have enough money to bet, have $" + Main.minWager + " on us.");
 			
-			Main.playerMoney += Main.minWager; // gives player minimum wager amount
-			System.out.println("\nCurrent Wallet: $" + Main.playerMoney + "\n");
+			user.setMoney(user.getMoney() + Main.minWager); // gives player minimum wager amount
+			System.out.println("\nCurrent Wallet: $" + user.getMoney() + "\n");
+			
 		}
 		
 		System.out.println("Place a wager on this game: ");
 
 		user.getHand(0).setWager(Main.getIntFromScannerRanged(scnr, Main.minWager, Main.maxWager)); // asks for wager between min / max
 		
-		if(user.getHand(0).getWager() > Main.playerMoney) { // if wager is greater than the money they have, get num between min and player money
+		if(user.getHand(0).getWager() > user.getMoney()) { // if wager is greater than the money they have, get num between min and player money
 			
 			System.out.println("Slow down there, you don't have enough cash for that bet.");
-			user.getHand(0).setWager(Main.getIntFromScannerRanged(scnr, Main.minWager, Main.playerMoney));
+			user.getHand(0).setWager(Main.getIntFromScannerRanged(scnr, Main.minWager, user.getMoney()));
 			
 		}
 		
-		Main.playerMoney -= user.getHand(0).getWager();
+		user.setMoney(user.getMoney() - user.getHand(0).getWager());
 
-		System.out.println("Current Wallet: $" + Main.playerMoney + "\n");
+		System.out.println("Current Wallet: $" + user.getMoney() + "\n");
 		
-		for(int i = 0; i < cpuList.size(); i++) {
+		System.out.println(Main.playerName + " wagered $" + user.getHand(0).getWager() + "\n");
+		
+		for(int i = 0; i < cpuList.size(); i++) { // start cpu wagers
 			
-			int wager = rng.nextInt((Main.maxWager - Main.minWager)) + Main.minWager;
+			int wager;
+			
+			do {
+				
+				wager = rng.nextInt((Main.maxWager - Main.minWager)) + Main.minWager;
+				
+			} while(wager > cpuList.get(i).getMoney());
+			
+			
 			
 			cpuList.get(i).getHand(0).setWager(wager);
+			//System.out.println("This cpu has $" + cpuList.get(i).getMoney());
 			System.out.println(cpuList.get(i).getName() + " wagered $" + wager + "\n");
 			
-		}
+			cpuList.get(i).setMoney(cpuList.get(i).getMoney() - wager);
+			
+		} // end cpu wagers
 		
+		Main.playerMoney = user.getMoney();
 	}
 	
 	public void dealCards() { //TODO deal cards to all players ( have an arraylist of cpuplayers thats cycled through?) (User -> Any CPUs -> Dealer)
